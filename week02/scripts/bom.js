6ea8a8a5e8d0d015cb3fd389edcce90aa2ebf08c
princegodwyll15 <godwyllprince@gmail.com>
2024-09-28 19:37:03 +0000

    update

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,23 @@
 const input = document.getElementById("favchap");
-// const addButton = document.querySelector('button');
-const button = document.querySelector('button[type="submit"]');
+const button = document.querySelector("button");
 const list = document.getElementById("list");
 
 const li = document.createElement("li");
 const deleteButton = document.createElement("button");
-list.textContent = input.value;
 
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-li.append(li);
+button.addEventListener("click", function () {
+  if (input.value.trim() !== " ") {
+    li.textContent = input.value;
+    deleteButton.textContent = "❌";
+    li.append(deleteButton);
+    list.append(li);
+  }
+});
+
+deleteButton.addEventListener("click", function () {
+  list.removeChild(li);
+  input.focus();
+});
+
+input.value(" ");
+input.focus();
