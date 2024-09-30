36368db1cbbf37f56076d73c7c99978c84294310
princegodwyll15 <godwyllprince@gmail.com>
2024-09-30 10:14:05 +0000

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,17 @@
 // last modification date
 const currentYear = new Date().getFullYear();
+document.getElementById("year").innerText = currentYear;
 ModifiedElement = document.getElementById("lastModification");
 const getLastModified = new Date(document.lastModified);
 actDate = getLastModified.toLocaleString();
 
 ModifiedElement.textContent = `Last updated: ${actDate}`;
+
+//hamburger on small screens
+const getNavigation = document.querySelector(".navigation");
+const openMenu = document.querySelector("#menu");
+
+openMenu.addEventListener("click", () => {
+  getNavigation.classList.toggle("open");
+  openMenu.classList.toggle("open");
+});
