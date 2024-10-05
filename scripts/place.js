c9336cda8efd5c9a9dfa41ab31442a7331f2c5c7
princegodwyll15 <godwyllprince@gmail.com>
2024-10-05 09:24:58 +0000

    update

--- scripts/place.js
+++ scripts/place.js
@@ -8,3 +8,38 @@ const lasModification = document.querySelector("#LastModification");
 
 year.textContent = `${currentYear} Prince Godwyll Accra Ghana.`;
 lasModification.textContent = getLastModified;
+
+const tempCelcius = 2;
+const tempFarend = 68;
+const windSpeed = 6;
+const windSpeedMph = 3.11;
+const getwindChill = document.getElementById("windChill");
+
+const calculateWindChill = () => {
+  let windChill;
+  if (tempCelcius <= 10 && windSpeed > 5) {
+    windChill =
+      (13.12 +
+      0.6215 * tempCelcius -
+      Math.pow(11.37 * windSpeed, 0.16) +
+      Math.pow(0.3965 * tempCelcius * windSpeed, 0.16)).toFixed(4);
+      
+    getwindChill.textContent = `Windchill: ${windChill} °C`;
+  }
+  
+  else if (tempFarend <= 50 && windSpeedMph > 3) {
+    windChill =
+      (13.12 +
+      0.6215 * tempFarend -
+      Math.pow(11.37 * windSpeedMph, 0.16) +
+      Math.pow(0.3965 * tempFarend * windSpeedMph, 0.16)).toFixed(4);
+      Math.round(windChill, 4)
+    getwindChill.textContent = `Windchill: ${windChill} °F`;
+  } 
+  
+  else {
+    getwindChill.textContent = "Windchill " + "N/A";
+  }
+};
+
+window.onload = calculateWindChill;
