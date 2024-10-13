83381fe028b367ccd9ff42654aa4d6d3f1717ab3
princegodwyll15 <godwyllprince@gmail.com>
2024-10-13 18:10:17 +0000

    update

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,3 +1,24 @@
+// last modification date
+const currentYear = new Date().getFullYear();
+const footerMsg = document.querySelector("#year");
+
+footerMsg.textContent = `${currentYear} Prince Godwyll Accra Ghana.`;
+ModifiedElement = document.getElementById("lastModification");
+const getLastModified = new Date(document.lastModified);
+actDate = getLastModified.toLocaleString();
+
+ModifiedElement.textContent = `Last updated: ${actDate}`;
+
+//hamburger on small screens
+const getNavigation = document.querySelector(".navigation");
+const openMenu = document.querySelector("#menu");
+
+openMenu.addEventListener("click", () => {
+  getNavigation.classList.toggle("open");
+  openMenu.classList.toggle("open");
+});
+
+
 const temples = [
   {
     templeName: "Aba Nigeria",
@@ -163,22 +184,3 @@ document
   .getElementById("home")
   .addEventListener("click", () => updateContainer());
 
-// last modification date
-const currentYear = new Date().getFullYear();
-const footerMsg = document.querySelector("#year");
-
-footerMsg.textContent = `${currentYear} Prince Godwyll Accra Ghana.`;
-ModifiedElement = document.getElementById("lastModification");
-const getLastModified = new Date(document.lastModified);
-actDate = getLastModified.toLocaleString();
-
-ModifiedElement.textContent = `Last updated: ${actDate}`;
-
-//hamburger on small screens
-const getNavigation = document.querySelector(".navigation");
-const openMenu = document.querySelector("#menu");
-
-openMenu.addEventListener("click", () => {
-  getNavigation.classList.toggle("open");
-  openMenu.classList.toggle("open");
-});
