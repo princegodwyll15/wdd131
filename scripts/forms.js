6ecf352b1b44925b214943b4cd00f36eb12988ec
princegodwyll15 <godwyllprince@gmail.com>
2024-10-24 13:50:14 -0700

    update

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,58 +1,54 @@
 const products = [
-    {
-      id: "fc-1888",
-      name: "flux capacitor",
-      averagerating: 4.5
-    },
-    {
-      id: "fc-2050",
-      name: "power laces",
-      averagerating: 4.7
-    },
-    {
-      id: "fs-1987",
-      name: "time circuits",
-      averagerating: 3.5
-    },
-    {
-      id: "ac-2000",
-      name: "low voltage reactor",
-      averagerating: 3.9
-    },
-    {
-      id: "jj-1969",
-      name: "warp equalizer",
-      averagerating: 5.0
-    }
-  ];
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5,
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7,
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5,
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9,
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0,
+  },
+];
 
-  
-  const selectElement = document.querySelector('#select-product');
-  
-  if (selectElement) {
-    const populateOptions = () => {
-      products.forEach((product) => {
-        const option = new Option(product.name, product.id);
-        selectElement.append(option);
-      });
-    };
-    populateOptions();
-  } else {
-    console.error("Select element not found.");
-  }
-    
+const selectElement = document.querySelector("#select-product");
+
+if (selectElement) {
+  const populateOptions = () => {
+    products.forEach((product) => {
+      const option = new Option(product.name, product.id);
+      selectElement.append(option);
+    });
+  };
+  populateOptions();
+} else {
+  console.error("Select element not found.");
+}
 
 // get all reqiured variables to populate the footer data
-  const newYear = document.querySelector("#year");
-  const lastModified = document.querySelector("#lastModification");
+const newYear = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModification");
 
-  // declare a new date
-  const year = new Date();
-  const currentYear = year.getFullYear();
-  newYear.textContent = `${currentYear} Prince Godwyll Ghana, Accra.`;
+// declare a new date
+const year = new Date();
+const currentYear = year.getFullYear();
+newYear.textContent = `${currentYear} Prince Godwyll Ghana, Accra.`;
 
-  const lastModification =new Date(document.lastModified);
-  const actModified = lastModification.toLocaleString();
-  lastModified.textContent = `Last Modified: ${actModified}`;
-
-  
+const lastModification = new Date(document.lastModified);
+const actModified = lastModification.toLocaleString();
+lastModified.textContent = `Last Modified: ${actModified}`;
