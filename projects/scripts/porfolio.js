a11c6efbb7a700354ed358bdd33e848aaced93f8
princegodwyll15 <godwyllprince@gmail.com>
2024-10-27 11:49:09 -0700

    update

--- projects/scripts/porfolio.js
+++ projects/scripts/porfolio.js
@@ -1,5 +1,8 @@
-// my-details = [
-//     {
-//         email:"godwyllprince@gmail.com"
-//     }
-// ];
+const mydetails = [
+    {
+        name:"Prince Godwyll",
+        email:"godwyllprince@gmail.com",
+        phone:"0595167828"
+    }
+];
+
