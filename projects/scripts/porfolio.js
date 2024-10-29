0b61affcddb7db7ee653cf77152d2166f8d5749b
princegodwyll15 <godwyllprince@gmail.com>
2024-10-29 09:11:56 +0000

    project

--- projects/scripts/porfolio.js
+++ projects/scripts/porfolio.js
@@ -6,3 +6,15 @@ const mydetails = [
     }
 ];
 
+// populate the footer element
+const getYear = document.querySelector('#year');
+const getLastModifeid = document.querySelector('#lastModification');
+
+const date = new Date();
+const year = date.getFullYear();
+const lastModifed = new Date(document.lastModified);
+const convertToString = lastModifed.toLocaleDateString();
+
+getYear.textContent = `© ${year} Prince Godwyll Accra, Ghana.`;
+getLastModifeid.textContent=`Last Modified: ${convertToString}`;
+
