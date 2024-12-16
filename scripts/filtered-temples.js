0213711a3b2f5fc453ec4ecd23693b49d4b6b8bf
princegodwyll15 <godwyllprince@mail.com>
2024-12-16 19:53:39 +0000

    nothing

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -6,7 +6,6 @@ footerMsg.textContent = `${currentYear} Prince Godwyll Accra Ghana.`;
 ModifiedElement = document.getElementById("lastModification");
 const getLastModified = new Date(document.lastModified);
 actDate = getLastModified.toLocaleString();
-
 ModifiedElement.textContent = `Last updated: ${actDate}`;
 
 //hamburger on small screens
